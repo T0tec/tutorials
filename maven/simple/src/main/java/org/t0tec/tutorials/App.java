4ab870b26f5323c1fdb27c4ab7fcdf182dce6fbe
t0tec <dep.michael@gmail.com>
2014-03-25 00:17:06 +0100

    Finished chapter 1-4

---
+++ maven/simple/src/main/java/org/t0tec/tutorials/App.java
@@ -0,0 +1,13 @@
+package org.t0tec.tutorials;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
