a0a53dd7ce8cb28e565d15e20bb3a28b40c348bd
t0tec <dep.michael@gmail.com>
2014-11-12 15:24:02 +0100

    Solved some exercises

--- scala/progfun/.worksheet/src/week1.session.scala
+++ scala/progfun/.worksheet/src/week1.session.scala
@@ -1,31 +1,62 @@
 package week1
 
-object session {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(40); val res$0 = 
-  1 + 2;System.out.println("""res0: Int(3) = """ + $show(res$0));$skip(44); 
-  def abs(x: Double) = if (x < 0) -x else x;System.out.println("""abs: (x: Double)Double""");$skip(335); 
+object session {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(42); val res$0 = 
+    1 + 2;System.out.println("""res0: Int(3) = """ + $show(res$0));$skip(46); 
+    def abs(x: Double) = if (x < 0) -x else x;System.out.println("""abs: (x: Double)Double""");$skip(374); 
 
-  def sqrt(x: Double) = {
-    def sqrtIter(guess: Double, x: Double): Double =
-      if (isGoodEnough(guess)) guess
-      else sqrtIter(improve(guess), x)
+    def sqrt(x: Double) = {
+        def sqrtIter(guess: Double, x: Double): Double =
+            if (isGoodEnough(guess)) guess
+            else sqrtIter(improve(guess), x)
 
-    def isGoodEnough(guess: Double) =
-      abs(guess * guess - x) / x < 0.001
+        def isGoodEnough(guess: Double) =
+            abs(guess * guess - x) / x < 0.001
 
-    def improve(guess: Double) =
-      (guess + x / guess) / 2
-      
-    sqrtIter(1.0, x)
-  };System.out.println("""sqrt: (x: Double)Double""");$skip(13); val res$1 = 
-	
-  sqrt(2);System.out.println("""res1: Double = """ + $show(res$1));$skip(10); val res$2 = 
-  sqrt(4);System.out.println("""res2: Double = """ + $show(res$2));$skip(13); val res$3 = 
-  sqrt(1e-6);System.out.println("""res3: Double = """ + $show(res$3));$skip(13); val res$4 = 
-  sqrt(1e60);System.out.println("""res4: Double = """ + $show(res$4))}
+        def improve(guess: Double) =
+            (guess + x / guess) / 2
 
-  // Q: Why is the isGoodEnough test not very precise for small numbers and can lead to non-termination for very large numbers?
-  // A: isGoodEnough not good for small numbers and for large numbers can be further apart from each other (absolute value)
-  // ==> added / x before x < 0.001
-  
-  // Scoping & Blocks ==> good for removing redundant variables and also shorter & cleaner code
+        sqrtIter(1.0, x)
+    };System.out.println("""sqrt: (x: Double)Double""");$skip(14); val res$1 = 
+
+    sqrt(2);System.out.println("""res1: Double = """ + $show(res$1));$skip(12); val res$2 = 
+    sqrt(4);System.out.println("""res2: Double = """ + $show(res$2));$skip(15); val res$3 = 
+    sqrt(1e-6);System.out.println("""res3: Double = """ + $show(res$3));$skip(15); val res$4 = 
+    sqrt(1e60);System.out.println("""res4: Double = """ + $show(res$4));$skip(513); 
+
+    // Q: Why is the isGoodEnough test not very precise for small numbers and can lead to non-termination for very large numbers?
+    // A: isGoodEnough not good for small numbers and for large numbers can be further apart from each other (absolute value)
+    // ==> added / x before x < 0.001
+
+    // Scoping & Blocks ==> good for removing redundant variables and also shorter & cleaner code
+
+    def gcd(a: Int, b: Int): Int = { // Tail recursive call
+        if (b == 0) a
+        else gcd(b, a % b)
+    };System.out.println("""gcd: (a: Int, b: Int)Int""");$skip(19); val res$5 = 
+
+    gcd(14, 21);System.out.println("""res5: Int = """ + $show(res$5));$skip(167); ;
+
+    def factorial(n: Int): Int = { // Non-tail recursice call because of the multiplication at the end
+        if (n == 0) 1
+        else n * factorial(n - 1)
+    };System.out.println("""factorial: (n: Int)Int""");$skip(19); val res$6 = 
+
+    factorial(4);System.out.println("""res6: Int = """ + $show(res$6));$skip(185); 
+
+    def factorial_tail_recursive(n: Int): Int = {
+        def loop(acc: Int, n: Int): Int =
+            if (n == 0) acc
+            else loop(acc * n, n - 1)
+        loop(1, n)
+    };System.out.println("""factorial_tail_recursive: (n: Int)Int""");$skip(35); val res$7 = 
+
+    factorial_tail_recursive(4);System.out.println("""res7: Int = """ + $show(res$7));$skip(123); ;
+
+    def sum(xs: List[Int]): Int = {
+        if (!xs.isEmpty) {
+            xs.head + sum(xs.tail)
+        } else 0
+    };System.out.println("""sum: (xs: List[Int])Int""");$skip(25); val res$8 = 
+
+    sum(List(1, 2, 3));System.out.println("""res8: Int = """ + $show(res$8))}
 }
--- scala/progfun/src/week1/session.sc
+++ scala/progfun/src/week1/session.sc
@@ -1,31 +1,62 @@
 package week1
 
 object session {
-  1 + 2                                           //> res0: Int(3) = 3
-  def abs(x: Double) = if (x < 0) -x else x       //> abs: (x: Double)Double
+    1 + 2                                         //> res0: Int(3) = 3
+    def abs(x: Double) = if (x < 0) -x else x     //> abs: (x: Double)Double
 
-  def sqrt(x: Double) = {
-    def sqrtIter(guess: Double, x: Double): Double =
-      if (isGoodEnough(guess)) guess
-      else sqrtIter(improve(guess), x)
+    def sqrt(x: Double) = {
+        def sqrtIter(guess: Double, x: Double): Double =
+            if (isGoodEnough(guess)) guess
+            else sqrtIter(improve(guess), x)
 
-    def isGoodEnough(guess: Double) =
-      abs(guess * guess - x) / x < 0.001
+        def isGoodEnough(guess: Double) =
+            abs(guess * guess - x) / x < 0.001
 
-    def improve(guess: Double) =
-      (guess + x / guess) / 2
-      
-    sqrtIter(1.0, x)
-  }                                               //> sqrt: (x: Double)Double
-	
-  sqrt(2)                                         //> res1: Double = 1.4142156862745097
-  sqrt(4)                                         //> res2: Double = 2.000609756097561
-  sqrt(1e-6)                                      //> res3: Double = 0.0010000001533016628
-  sqrt(1e60)                                      //> res4: Double = 1.0000788456669446E30
+        def improve(guess: Double) =
+            (guess + x / guess) / 2
 
-  // Q: Why is the isGoodEnough test not very precise for small numbers and can lead to non-termination for very large numbers?
-  // A: isGoodEnough not good for small numbers and for large numbers can be further apart from each other (absolute value)
-  // ==> added / x before x < 0.001
-  
-  // Scoping & Blocks ==> good for removing redundant variables and also shorter & cleaner code
+        sqrtIter(1.0, x)
+    }                                             //> sqrt: (x: Double)Double
+
+    sqrt(2)                                       //> res1: Double = 1.4142156862745097
+    sqrt(4)                                       //> res2: Double = 2.000609756097561
+    sqrt(1e-6)                                    //> res3: Double = 0.0010000001533016628
+    sqrt(1e60)                                    //> res4: Double = 1.0000788456669446E30
+
+    // Q: Why is the isGoodEnough test not very precise for small numbers and can lead to non-termination for very large numbers?
+    // A: isGoodEnough not good for small numbers and for large numbers can be further apart from each other (absolute value)
+    // ==> added / x before x < 0.001
+
+    // Scoping & Blocks ==> good for removing redundant variables and also shorter & cleaner code
+
+    def gcd(a: Int, b: Int): Int = { // Tail recursive call
+        if (b == 0) a
+        else gcd(b, a % b)
+    }                                             //> gcd: (a: Int, b: Int)Int
+
+    gcd(14, 21);                                  //> res5: Int = 7
+
+    def factorial(n: Int): Int = { // Non-tail recursice call because of the multiplication at the end
+        if (n == 0) 1
+        else n * factorial(n - 1)
+    }                                             //> factorial: (n: Int)Int
+
+    factorial(4)                                  //> res6: Int = 24
+
+    def factorial_tail_recursive(n: Int): Int = {
+        def loop(acc: Int, n: Int): Int =
+            if (n == 0) acc
+            else loop(acc * n, n - 1)
+        loop(1, n)
+    }                                             //> factorial_tail_recursive: (n: Int)Int
+
+    factorial_tail_recursive(4);                  //> res7: Int = 24
+
+    def sum(xs: List[Int]): Int = {
+        if (!xs.isEmpty) {
+            xs.head + sum(xs.tail)
+        } else 0
+    }                                             //> sum: (xs: List[Int])Int
+
+    sum(List(1, 2, 3))                            //> res8: Int = 6
 }
